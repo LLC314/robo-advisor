bc55dc813ecbe72bd17bc24703e12e8abd1a3900
LLC314 <84344987+LLC314@users.noreply.github.com>
2021-06-10 20:13:06 -0400

    Fetch data from API

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,16 +1,19 @@
-print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
-print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
-print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
-print("-------------------------")
-print("HAPPY INVESTING!")
-print("-------------------------")
+
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+
+symbol = "MSFT"
+request_url ="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+
+
+response = requests.get(request_url)
+print(type(response))
+print(response.text)
