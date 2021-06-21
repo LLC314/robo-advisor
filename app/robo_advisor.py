61acf0d219e13ebf798c34d96a26945cb67bd0fa
LLC314 <84344987+LLC314@users.noreply.github.com>
2021-06-21 18:34:04 -0400

    Request Date and Time

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -2,18 +2,28 @@
 import requests
 import os
 from dotenv import load_dotenv
+import pandas as pd
+import datetime
+
+date_time = datetime.datetime.now()
 
 load_dotenv()
-
-
 
 ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
 
-symbol = "MSFT"
-request_url ="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+symbol = input("Please enter a stock symbol: ")
 
+train = pd.read_table("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv")
+train.head()
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv"
 
 response = requests.get(request_url)
-print(type(response))
-print(response.text)
+print("------------------------------")
+print("You selected:",symbol)
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: ", date_time)
+# print(type(response))
+# print(response.text)
